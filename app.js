eb6e88dbb67ff144b57b6a3d2cabdd96bd5f4438
enrlopez <102252933+enrlopez@users.noreply.github.com>
2022-03-28 13:25:01 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,32 @@
 
-uuug
+function askName() {
+    var name = prompt ("whats your name:")
+    return name
+}
+
+function temperature() {
+  var temp = prompt ('What is the temp')
+  return temp
+}
+
+function CtoF(x) {
+    var newtemp = x * 9 / 5 * 32
+    return newtemp
+}
+
+function FtoC(x) {
+  var newtemp = x - 32 * 5 / 9
+  return newtemp
+}
+
+function daddyFunc(x){
+  var choice = prompt ("what do you want to convert C to F or F to C?")
+  while (choice != "C to F" && choice !=  "F to C" ) {
+  choice = prompt ("only enter C to F or F to C?")
+  }
+  if (choice == "C to F" ) {
+    alert(`hi ${x} the temperature converted from ${choice} ${CtoF} `)
+  } else if ("F to C") {
+    ${FtoC}   
+  } 
+} 
